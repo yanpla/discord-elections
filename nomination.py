2ccdb69484e4b25920bb7cc39c6b19b8ac6d0bad
yanpla <yannick@plantenga.com>
2024-07-23 01:09:35 +0200

    Added check for deleted file and removed files

--- nomination.py
+++ nomination.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from datetime import datetime
 
 class Nomination:
@@ -6,12 +7,18 @@ class Nomination:
         self.csv_file = "nominees.csv"
         self.votes_csv = "votes.csv"
 
+    def check_and_create_file(self, file_path):
+        if not os.path.exists(file_path):
+            open(file_path, 'w').close()
+
     def nominate_candidate(self, candidate):
+        self.check_and_create_file(self.csv_file)
         with open(self.csv_file, mode='a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow([candidate.id, candidate.display_name])
 
     def get_nominations(self):
+        self.check_and_create_file(self.csv_file)
         nominations = []
         with open(self.csv_file, mode='r') as file:
             reader = csv.reader(file)
@@ -21,9 +28,11 @@ class Nomination:
         return nominations
 
     def clear_nominations(self):
+        self.check_and_create_file(self.csv_file)
         open(self.csv_file, 'w').close()  # Clear the contents of the CSV file
 
     def is_candidate_nominated(self, candidate):
+        self.check_and_create_file(self.csv_file)
         with open(self.csv_file, mode='r') as file:
             reader = csv.reader(file)
             for row in reader:
@@ -35,9 +44,11 @@ class Nomination:
         return datetime.now().weekday() < 5
     
     def clear_votes(self):
+        self.check_and_create_file(self.votes_csv)
         open(self.votes_csv, 'w').close()
 
     def record_vote(self, voter, nominee_id):
+        self.check_and_create_file(self.votes_csv)
         rows = []
         voter_id = str(voter.id)
         with open(self.votes_csv, mode='r') as file:
@@ -54,6 +65,7 @@ class Nomination:
                 writer.writerow(row)
 
     def get_votes(self):
+        self.check_and_create_file(self.votes_csv)
         votes = {}
         with open(self.votes_csv, mode='r') as file:
             reader = csv.reader(file)
